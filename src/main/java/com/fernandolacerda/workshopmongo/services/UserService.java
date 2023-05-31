a5dfacded7305e50f4ab233b9874c13cab96c748
fernandolacerda1746 <stingeralfa@gmail.com>
2023-05-31 12:10:52 -0300

    Deleção de Usuário com DELETE

--- src/main/java/com/fernandolacerda/workshopmongo/services/UserService.java
+++ src/main/java/com/fernandolacerda/workshopmongo/services/UserService.java
@@ -36,6 +36,13 @@ public class UserService {
 		
 	}
 	
+	public void delete(String id) {
+		
+		findById(id);
+		repo.deleteById(id);
+		
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		
 		return new User(objDto.getId(), objDto.getUser(), objDto.getEmail());
