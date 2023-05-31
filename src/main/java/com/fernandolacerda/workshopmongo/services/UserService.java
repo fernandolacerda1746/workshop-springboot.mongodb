8d9874607d90a71ba4cd27fa1e0826766f25f9d5
fernandolacerda1746 <stingeralfa@gmail.com>
2023-05-31 17:21:34 -0300

    Atualização de Usuário com PUT

--- src/main/java/com/fernandolacerda/workshopmongo/services/UserService.java
+++ src/main/java/com/fernandolacerda/workshopmongo/services/UserService.java
@@ -43,6 +43,21 @@ public class UserService {
 		
 	}
 	
+	public User update(User obj) {
+		
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		
+	}
+	
+	private void updateData(User newObj, User obj) {
+		
+		newObj.setUser(obj.getUser());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		
 		return new User(objDto.getId(), objDto.getUser(), objDto.getEmail());
